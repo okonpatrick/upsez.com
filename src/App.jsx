589ea30b98d9ddc051f5a335f63f2fc13a726050
okonpatrick <okonpatrick2000@gmail.com>
2024-03-16 09:17:40 -0700

    sixth changes

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Navbar from './components/LocalNavbar';
+import SearchForm from './components/Forms/SearchBar';
+import AboutUs from './components/AboutUs';
+import Footer from './components/Footer';
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <SearchForm/>
+      <AboutUs/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,25 @@
+
+export default function AboutUs() {
+  return (
+    <div>
+    <div className="sm:flex items-center max-w-screen-xl">
+        <div className="sm:w-1/2 p-10">
+            <div className="image object-center text-center">
+                <img src="https://i.imgur.com/WbQnbas.png"/>
+            </div>
+        </div>
+        <div className="sm:w-1/2 p-5">
+            <div className="text">
+                <span className="text-black border-b-2 border-orange-600 uppercase">About us</span>
+                <h2 className="my-4 font-bold text-3xl  sm:text-4xl ">About <span className="text-orange-600">Our Company</span>
+                </h2>
+                <p className="text-black">
+                Upsez Internet Shopping in Nigeria - Finest Shopping Site
+    Upsez .com stands as Nigeria’s top online Shopping hub. We take pride in offering a comprehensive array of goods for life and living, all at unbeatable prices. Our access to Original Equipment Manufacturers and top-tier vendors grants us a vast selection of items at highly competitive rates. Among our popular sections are gadgets, cell phones, computing devices, fashion, cosmetics, fabrics, and much more, sourced from leading brands. Additional categories encompass laptops, smartphones, earbuds, mobile accessories, musical instruments, fitness gear, and more. To ensure a seamless and delightful shopping experience, we provide perks like discounts, consumer promotions spanning various categories, and hassle-free bulk purchases with doorstep delivery. Benefit from complimentary shipping for select products, and with bulk purchases, enjoy reduced shipping costs, discounted rates, and flexible payment options. Our platform supports debit card payments, offering a convenient and secure payment method. Explore a diverse range of lifestyle services online. Mark your calendar for our annual special sales events and don’t miss out on the biggest online deals.
+                </p>
+            </div>
+        </div>
+    </div></div>
+  )
+}
+
